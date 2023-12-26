80e7142d1125895414b49d811b47ca81ded5c515
Hau Tran <tranleconghau772001@gmail.com>
2023-12-27 06:42:06 +0700

    build factory pattern to create product by type

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const ProductService = require('../services/product.service');
+const { OK, CREATED, SuccessResponse } = require('../core/success.response');
+class ProductController {
+  createProduct = async (req, res, next) => {
+    new SuccessResponse({
+      message: 'Create new product Successfully!',
+      metadata: await ProductService.createProduct(
+        req.body.product_type,
+        req.body,
+      ),
+    }).send(res);
+  };
+}
+
+module.exports = new ProductController();
