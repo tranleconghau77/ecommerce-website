9d80523d9524ff36e0c7205c32c4a274f70834a3
Hau Tran <tranleconghau772001@gmail.com>
2024-01-27 23:18:27 +0700

    init acquireLock

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -6,3 +6,14 @@ const redisClient = redis.createClient();
 
 const pexpire = promisify(redisClient.pExpire).bind(redisClient);
 const setnxAsync = promisify(redisClient.set).bind(redisClient);
+
+const acquireLock = async (productId, quantity, cartId) => {
+  const key = `lock_v2024_${productId}`;
+  const retryTimes = 10;
+  const expireTime = 3000;
+
+  for (let i = 0; i < retryTimes.length; i++) {
+    const result = await setnxAsync(key, expireTime); // check key if exist return 0 else return 1
+    console.log(`result:::`, result);
+  }
+};
