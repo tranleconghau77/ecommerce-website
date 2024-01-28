fd9c5228fcaa543601c02875902a99fb54a8dafc
Hau Tran <tranleconghau772001@gmail.com>
2024-01-28 22:45:36 +0700

    add release lock func

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -7,7 +7,7 @@ const redisClient = redis.createClient();
 const pexpire = promisify(redisClient.pExpire).bind(redisClient);
 const setnxAsync = promisify(redisClient.set).bind(redisClient);
 
-const acquireLock = async (productId, quantity, cartId) => {
+const acquiredLock = async (productId, quantity, cartId) => {
   const key = `lock_v2024_${productId}`;
   const retryTimes = 10;
   const expireTime = 3000;
@@ -15,5 +15,17 @@ const acquireLock = async (productId, quantity, cartId) => {
   for (let i = 0; i < retryTimes.length; i++) {
     const result = await setnxAsync(key, expireTime); // check key if exist return 0 else return 1
     console.log(`result:::`, result);
+
+    if (result == 1) {
+      // work with inventory
+      return key;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 50));
+    }
   }
+};
+
+const releaseLock = async (keyLock) => {
+  const delAsyncKey = promisify(redisClient.del).bind(redisClient);
+  return await delAsyncKey(keyLock);
 };
