7bab6cc68d1cd757e9f3b9cf341676a8bf9e9fe8
Hau Tran <tranleconghau772001@gmail.com>
2024-02-16 21:55:52 +0700

    add comment to notification model

--- src/models/notification.model.js
+++ src/models/notification.model.js
@@ -5,9 +5,14 @@ const { model, Schema } = require('mongoose');
 const DOCUMENT_NAME = 'Notification';
 const COLLECTION_NAME = 'Notifications';
 
+// ORDER-001: order successfully
+// ORDER-002: order failed
+// PROMOTION-001: new promotion
+// SHOP-001: new product by User following
+
 const notificationSchema = new Schema(
   {
-    noti_type: { type: String, enum: ['order', 'product'] },
+    noti_type: { type: String, enum: ['ORDER-001', 'ORDER-002'] },
   },
   {
     timestamps: true,
