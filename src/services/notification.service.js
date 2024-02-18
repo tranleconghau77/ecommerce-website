f4038ea010256b26cfa93df3011700b8128ae29b
Hau Tran <tranleconghau772001@gmail.com>
2024-02-19 06:25:53 +0700

    finish basic notification service

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -1,8 +1,32 @@
 'use strict';
 
+const notificationModel = require('../models/notification.model');
+
 const pushNotiToSystem = async ({
   type = 'SHOP-001',
   receivedId = 1,
   senderId = 1,
   options = {},
-}) => {};
+}) => {
+  let noti_content;
+
+  if (type === 'SHOP-001') {
+    noti_content = '@@@ added new product: @@@';
+  } else if (type === 'PROMOTION-001') {
+    noti_content = '@@@ added new voucher: @@@';
+  }
+
+  const newNoti = await notificationModel.create({
+    noti_type: type,
+    noti_content,
+    noti_senderId: senderId,
+    noti_receivedId: receivedId,
+    noti_options: options,
+  });
+
+  return newNoti;
+};
+
+module.exports = {
+  pushNotiToSystem,
+};
